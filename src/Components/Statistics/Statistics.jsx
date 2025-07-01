28de34d5aeb3fc5b2c85a3d352bbc674ca442b7d
joseamorim <jpoa95@gmail.com>
2025-07-01 16:40:01 +0100

    01/07

---
+++ src/Components/Statistics/Statistics.jsx
@@ -0,0 +1,22 @@
+
+export default function Statistics() {
+
+    return (
+        <div className="statisticsMainDiv">
+            <div>year</div>
+            <div>month</div>
+            <div>day</div>
+            <div>project</div>
+            <div>equipment</div>
+            <div>codea</div>
+            <div>codeb</div>
+            <div>codepr</div>
+            <div>codeps</div>
+            <div>codedr</div>
+            <div>start/middle/end</div>
+            <div>standard/hybrid/prm</div>
+            <div>a/b</div>
+            <div>in/out</div>
+        </div>
+    )
+}
